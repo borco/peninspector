eeab02d004667b4550f3de887a790ac5d3a420b8
Ioan Calin Borcoman <iborco@gmail.com>
2022-12-08 10:44:29 +0200

    force INI format

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,9 +1,17 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QSettings>
 
 int main(int argc, char *argv[])
 {
+    QGuiApplication::setOrganizationName("Ioan Calin");
+    QGuiApplication::setOrganizationDomain("com.gitlab.iborco");
+    QGuiApplication::setApplicationName("pendoctor");
+
+    QSettings::setDefaultFormat(QSettings::IniFormat);
+    qDebug() << "Settings file:" << QSettings().fileName();
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
